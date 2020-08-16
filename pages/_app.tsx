833bbefc8f7e967ac5477b691e44f55753093653
sshcrack <hendrik.lind777@gmail.com>
2020-08-16 22:45:22 +0000

    Fixed an issue which occured at building the website

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 
 
 // import App from "next/app";
-import type { AppProps } from 'next/app'
+import { AppProps } from 'next/app'
 import "../styles/globals.css"
 
 function MyApp({ Component, pageProps }: AppProps) {
