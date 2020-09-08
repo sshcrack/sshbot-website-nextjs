bd8e86ba9d336f307c0e8bc7a29a29c420f9e894
sshcrack <hendrik.lind777@gmail.com>
2020-09-08 19:21:04 +0200

    Added OG: meta

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,4 +1,3 @@
-//import Link from 'next/link'
 import Head from 'next/head';
 import React, { ReactNode } from 'react';
 import Nav from "./Nav";
@@ -9,22 +8,27 @@ type Props = {
 }
 
 const Layout = ({ children, title = 'Title not given.' }: Props) => (
-  <div id={"content-wrapper"}>
-
+  <div>
     <Head>
       <title>{title}</title>
       <meta charSet="utf-8" />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      <meta property="og:title" content="sshbot" />
+      <meta property="og:url" content="https://sshbot.ddnss.de" />
+      <meta property="og:image" content="https://sshbot.ddnss.de/imgs/og_image.png" />
     </Head>
-    <Nav></Nav>
 
-    <div id={"page-container"} className={"center"}>
-      {children}
+    <div id={"content-wrapper"}>
+      <Nav></Nav>
+
+      <div id={"page-container"} className={"center"}>
+        {children}
+      </div>
+
+      <footer>
+        <span>© 2020 sshbot</span>
+      </footer>
     </div>
-
-    <footer>
-      <span>© 2020 sshbot</span>
-    </footer>
   </div>
 )
 
