948531dcf04cf1d6d54d1d6936f4e9a95ab74180
sshcrack <hendrik.lind777@gmail.com>
2020-11-24 21:26:29 +0100

    I hope this is the first commit in this branch

--- components/Modes/Basic.tsx
+++ components/Modes/Basic.tsx
@@ -2,6 +2,6 @@ import ModeProps from 'utils/modeProps';
 
 export const Basic = ({data}: ModeProps) => (
   <div>
-    <span>Work in Progress</span>
+    <span>Data {JSON.stringify(data)}</span>
   </div>
 )
