70f9ad1a04b796142cee369a279cc8a83e94f724
sshcrack <hendrik.lind777@gmail.com>
2020-11-25 15:18:34 +0100

    Fixed env variables

--- pages/redirects/guild.tsx
+++ pages/redirects/guild.tsx
@@ -2,6 +2,7 @@ import { useRouter } from 'next/router';
 import { parseCookies } from 'nookies';
 import { useEffect, useState } from 'react';
 import io from "socket.io-client";
+import { isNull } from 'utils/tools';
 
 let socket: SocketIOClient.Socket;
 const clientID = process.env.CLIENT_ID;
@@ -52,7 +53,7 @@ function SocketPage() {
 
   if (data?.connected) socket?.emit("listen", cookies.botSession)
   if (data?.registered && typeof window !== "undefined")
-    window.location.href = `https://discord.com/oauth2/authorize?client_id=${clientID}&scope=bot&permissions=8&response_type=code&redirect_uri=${redirectUrl}/redirects/close?mode=bot&guild_id=${id}&disable_guild_select=true`
+    window.location.href = `https://discord.com/oauth2/authorize?client_id=${clientID}&scope=bot&permissions=8&response_type=code&redirect_uri=${isNull(redirectUrl) ? `${location.protocol}://${location.host}` : redirectUrl}/redirects/close?mode=bot&guild_id=${id}&disable_guild_select=true`
 
   return <>
     <h1>{data?.data}</h1>
