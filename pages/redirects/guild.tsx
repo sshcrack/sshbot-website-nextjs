35a7e376b34da94c89343dff338290284379f46c
sshcrack <hendrik.lind777@gmail.com>
2020-11-25 15:09:49 +0100

    Forgot to add redirect url

--- pages/redirects/guild.tsx
+++ pages/redirects/guild.tsx
@@ -5,6 +5,7 @@ import io from "socket.io-client";
 
 let socket: SocketIOClient.Socket;
 const clientID = process.env.CLIENT_ID;
+const redirectUrl = process.env.NEXTAUTH_URL;
 
 function SocketPage() {
   const [data, setData] = useState<Data>({
@@ -51,7 +52,7 @@ function SocketPage() {
 
   if (data?.connected) socket?.emit("listen", cookies.botSession)
   if (data?.registered && typeof window !== "undefined")
-    window.location.href = `https://discord.com/oauth2/authorize?client_id=${clientID}&scope=bot&permissions=8&response_type=code&redirect_uri=http://localhost:3000/redirects/close?mode=bot&guild_id=${id}&disable_guild_select=true`
+    window.location.href = `https://discord.com/oauth2/authorize?client_id=${clientID}&scope=bot&permissions=8&response_type=code&redirect_uri=${redirectUrl}/redirects/close?mode=bot&guild_id=${id}&disable_guild_select=true`
 
   return <>
     <h1>{data?.data}</h1>
